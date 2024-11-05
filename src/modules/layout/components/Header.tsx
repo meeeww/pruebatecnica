1953e067787a4d59f6a7a8f3851cc0d719a183c4
Zas <thezasm03@gmail.com>
2024-11-05 15:36:02 +0100

    Cambios

--- src/modules/layout/components/Header.tsx
+++ src/modules/layout/components/Header.tsx
@@ -5,16 +5,16 @@ const Header: FC = () => {
     <header className="flex items-center justify-between text-white p-8 h-[4.2vw] border-gray-900 border-b-4">
       {/* Logo */}
       <nav className="flex">
-        <img src="/Logo.svg" />
+        <a href="/"><img src="/Logo.svg" /></a>
       </nav>
 
       {/* Información Usuario */}
-      <nav className="flex gap-4">
-        <div>Home</div>
-        <div>Descrubrir</div>
-        <div>Favoritos</div>
-        <div>Subir Obra</div>
-        <div>Foto</div>
+      <nav className="flex gap-8 items-center">
+        <button className="bg-[#54399F85] w-[6.313rem] h-[2.438rem] rounded-[6.25rem] flex justify-center items-center gap-2 font-regular text-gray-100 tracking-normal text-lg"><img src="/icons/Home.svg" />Home</button>
+        <a href="#" className="font-thin text-gray-100 tracking-normal text-lg">Descubrir</a>
+        <a href="#" className="font-thin text-gray-100 tracking-normal text-lg">Favoritos</a>
+        <button className="bg-[#7855DC] w-[8.063rem] h-[2.438rem] rounded-[6.25rem] flex justify-center items-center gap-2 font-regular text-gray-100 tracking-normal text-lg">Subir Obra</button>
+        <button><img src="https://i.scdn.co/image/ab67616d0000b273211e6d4c3a41019e9309686d" alt="Perfil" className="w-10 h-10 rounded-full" /></button>
       </nav>
     </header>
   );
--- src/modules/music/components/FeaturedSongs.tsx
+++ src/modules/music/components/FeaturedSongs.tsx
@@ -8,8 +8,8 @@ type Song = {
   primary_color: string;
   track: {
     name: string;
-    artists: { name: string; id: string }[];
-    album: { name: string; images: { url: string }[] };
+    artists: { name: string; id: string; external_urls: { spotify: string } }[];
+    album: { name: string; images: { url: string }[]; external_urls: { spotify: string } };
   };
   video_thumbnail: { url: string | null };
 };
@@ -89,19 +89,23 @@ const FeaturedSongs: FC = () => {
           songs &&
           songs.map((song: Song, index: number) => (
             <div key={index} className="flex-none w-[15.125rem] h-[18.125rem] flex flex-col gap-3 pb-4 bg-[#211626] rounded-2xl">
-              <div className="relative w-full h-[12rem]">
-                <img
-                  src={song.track.album.images[0].url}
-                  alt={song.track.album.name}
-                  className="absolute top-0 left-0 w-full h-full object-cover rounded-t-lg"
-                />
-              </div>
+              <a href={song.track.album.external_urls.spotify} target="_blank" rel="noopener noreferrer">
+                <div className="relative w-full h-[12rem]">
+                  <img
+                    src={song.track.album.images[0].url}
+                    alt={song.track.album.name}
+                    className="absolute top-0 left-0 w-full h-full object-cover rounded-t-lg"
+                  />
+                </div>
+              </a>
               <div className="px-4 flex flex-col gap-3">
                 <h3 className="font-medium text-gray-100 tracking-wide text-2xl overflow-hidden text-ellipsis whitespace-nowrap">{song.track.name}</h3>
-                <div className="flex items-center w-full gap-2 text-gray-100">
-                  <img src={song.track.album.images[0].url} alt={song.track.artists[0].name} className="w-5 h-5 rounded-full" />
-                  <p className="text-gray-300 font-medium text-sm">{song.track.artists[0].name}</p>
-                </div>
+                <a href={song.track.artists[0].external_urls.spotify} target="_blank" rel="noopener noreferrer">
+                  <div className="flex items-center w-full gap-2 text-gray-100">
+                    <img src={song.track.album.images[0].url} alt={song.track.artists[0].name} className="w-5 h-5 rounded-full" />
+                    <p className="text-gray-300 font-medium text-sm">{song.track.artists[0].name}</p>
+                  </div>
+                </a>
               </div>
             </div>
           ))
